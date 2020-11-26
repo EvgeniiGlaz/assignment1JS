c6b916a4811d968a384acdd2c202316fe49b1b29
EvgeniiGlaz <68857634+EvgeniiGlaz@users.noreply.github.com>
2020-11-26 12:42:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+//Task 1
+var totalSum = 0;
+
+function sum(num) {
+	totalSum += num;
+	return sum;
+}
+
+sum(5)(3)
+
+console.log(totalSum)
+
+//Task 2
+let obj1 = {
+	num: 123,
+	str: 'privet',
+	innerObj: {
+		objNum: 123
+	},
+	innerArr: ['a', 'b'],
+	sayHi() {
+		console.log('hello')
+	}
+}
+
+function newObject(obj) {
+	b = {}
+	for (let key in obj) {
+		if (typeof obj[key] == 'object') {
+			try {
+				b[key] = [...obj[key]]
+			} catch (err) {
+				b[key] = { ...obj[key] }
+			}
+		} else {
+			b[key] = obj[key]
+		}
+	}
+	b.num = 321; //ПРОВЕРКИ ДЛЯ РАЗНЫХ ТИПОВ ДАННЫХ
+	b.str = 'tevirp';
+	b.innerObj.objNum = 4321;
+	b.innerArr[0] = 'f';
+	b.innerArr[1] = 'z';
+	b.sayHi = function (a) {
+		console.log(a)
+	}
+	return b;
+}
+console.log('Old object: ', obj1)
+console.log('New object: ', newObject(obj1))
+
