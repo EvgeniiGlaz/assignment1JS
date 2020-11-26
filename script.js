6a3767735b1bc8a94363cac485d60feea828ae13
EvgeniiGlaz <68857634+EvgeniiGlaz@users.noreply.github.com>
2020-11-26 12:51:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -36,14 +36,14 @@ function newObject(obj) {
 			b[key] = obj[key]
 		}
 	}
-	b.num = 321; //ПРОВЕРКИ ДЛЯ РАЗНЫХ ТИПОВ ДАННЫХ
-	b.str = 'tevirp';
-	b.innerObj.objNum = 4321;
-	b.innerArr[0] = 'f';
-	b.innerArr[1] = 'z';
-	b.sayHi = function (a) {
-		console.log(a)
-	}
+	//b.num = 321; //ПРОВЕРКИ ДЛЯ РАЗНЫХ ТИПОВ ДАННЫХ
+	//b.str = 'tevirp';
+	//b.innerObj.objNum = 4321;
+	//b.innerArr[0] = 'f';
+	//b.innerArr[1] = 'z';
+	//b.sayHi = function (a) {
+	//console.log(a)
+	//}
 	return b;
 }
 console.log('Old object: ', obj1)
